9647fca155075ea1c0db02f13478ce5931042418
jackiegatwiri <jaciegatz@gmail.com>
2019-03-07 14:43:15 +0300

    add landing page

--- js/location.js
+++ js/location.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   $("#but").click(function(event) {
+    var message = $("#message").val();
+    if (message == "") {
+      alert("Please State your Problem!!");
+      return;
+    }
     event.preventDefault();
     $(".myForm").hide();
     $(".dropdown").show();
   });
-  $("#cbd").click(function(){
+  $("#cbd").click(function() {
     $("#town").show();
     $(".dropdown").hide();
   });
-  $("#thikaTown").click(function(){
+  $("#thikaTown").click(function() {
     $("#thika").show();
     $("#town").hide();
     $(".dropdown").hide();
   });
+
 });
